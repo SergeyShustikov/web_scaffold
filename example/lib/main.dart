fa223c3301d773f4251236a264aedbfed19452cd
Sergey Shustikov <pandarium.shustikov@gmail.com>
2023-03-27 03:17:43 +0300

    Added examples and homepage. Bump version to 0.0.2

---
+++ example/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:web_scaffold/web_scaffold.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: ExampleUsage(),
+    );
+  }
+}
+
+class ExampleUsage extends StatelessWidget {
+  const ExampleUsage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WebScaffold(
+      bodyConfiguration: const BodyConfiguration(
+        [
+          FlexPart(1),
+          BodyPart(5),
+          FlexPart(1),
+        ],
+      ),
+      header: _buildHeader(),
+      body: _buildBody(),
+      footer: _buildFooter(),
+      linkConfiguration: true,
+      pinnedHeader: true,
+    );
+  }
+
+  Container _buildFooter() {
+    return Container(
+      height: kToolbarHeight,
+      color: Colors.yellow,
+      child: const Center(
+        child: Text(
+          'Footer',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Container _buildBody() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 16),
+      height: 4000,
+      color: Colors.blue,
+      child: const Center(
+        child: Text(
+          'Body',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Container _buildHeader() {
+    return Container(
+      color: Colors.red,
+      child: const Center(
+        child: Text(
+          'Header',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
